d89e5c4af5a883ac8705b8d8ca02c80430eb7503
ledavid0520 <ledavis275@gmail.com>
2019-07-25 22:36:37 -0500

    COMMIT: Calculate distance with testing data

--- front-end-servicios/src/app/servicio/service-map/service-map.component.ts
+++ front-end-servicios/src/app/servicio/service-map/service-map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { AgmCoreModule, MapsAPILoader } from '@agm/core';
-import {} from 'googlemaps';
+import { MapsAPILoader } from '@agm/core';
+declare var google;
 
 
 
@@ -10,33 +10,48 @@ import {} from 'googlemaps';
   styleUrls: ['./service-map.component.css']
 })
 export class ServiceMapComponent implements OnInit, OnChanges {
-  
+
   @Input('serviceInformation') serviceInformation;
 
-  title: string = 'Detalles Servicio';
-  latOrigen: number = 4.624335;
-  lngOrigen: number = -74.033644;
+  title = 'Detalles Servicio';
+  latOrigen = 4.624335;
+  lngOrigen = -74.033644;
 
-  latDestino: number = 5.535280;
-  lngDestino: number = -73.367780;
+  latDestino = 5.535280;
+  lngDestino = -73.367780;
 
-  constructor() { }
+  zoom = 7;
+
+  origin: any;
+  destination: any;
+  distance: any;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader
+  ) { }
 
   ngOnInit() {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if(changes.serviceInformation){
-      if(changes.serviceInformation.currentValue){
+    if (changes.serviceInformation) {
+      if (changes.serviceInformation.currentValue) {
         this.openMap();
       }
     }
   }
 
   openMap() {
-    // const a = new google.maps.LatLng(this.latOrigen, this.lngOrigen);
-    // const b = new google.maps.LatLng(this.latDestino, this.lngDestino);
-    // const distance = google.maps.geometry.spherical.computeDistanceBetween(a, b);
+    this.origin = { lat: this.latOrigen, lng: this.lngOrigen };
+    this.destination = { lat: this.latDestino, lng: this.lngDestino };
+
+    this.mapsAPILoader.load().then(() => {
+      const origin = new google.maps.LatLng(this.latOrigen, this.lngOrigen);
+      const destination = new google.maps.LatLng(this.latDestino, this.lngDestino);
+
+      this.distance = google.maps.geometry.spherical.computeDistanceBetween(origin, destination) / 1000;
+
+    });
   }
 
 }
